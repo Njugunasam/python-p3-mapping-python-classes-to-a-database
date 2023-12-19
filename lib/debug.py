84e053535bb288267e1433f7f93efaa7bbf574d6
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-19 19:50:12 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,17 @@
 
 from config import CONN, CURSOR
 from song import Song
+Song.create_table()
+
+
+
+
+
+CURSOR.execute("PRAGMA table_info(songs)")
+table_info = CURSOR.fetchall()
+print("Table Info:", table_info)
 
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb
+    ipdb.set_trace()
